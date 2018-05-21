925d641e13d158d5b0a1aab633d4040a1d179cb1
davidv <david.vondrak@vanio.cz>
2018-05-21 14:02:51 +0200

    Toggle JS component improved - support for checkboxes and radios.

--- Resources/assets/js/components/Toggle.ts
+++ Resources/assets/js/components/Toggle.ts
@@ -2,6 +2,7 @@ import {component} from 'jquery-ts-components';
 
 interface ToggleOptions
 {
+    source: JQuery|HTMLElement|string;
     target: JQuery|HTMLElement|string;
     className: string;
 }
@@ -14,13 +15,28 @@ export default class Toggle
     public constructor(element: JQuery|HTMLElement|string, options: ToggleOptions|string)
     {
         this.options = $.extend(
-            {className: 'toggle--active'},
+            {className: 'toggle--active', source: element},
             typeof options === 'string' ? {target: options} : options
         );
 
-        $(element).on('click', (event: JQuery.Event) => {
-            event.preventDefault();
-            $(this.options.target).toggleClass(this.options.className);
-        });
+        const $source = $(this.options.source);
+        const $target = $(this.options.target);
+        const isSourceCheckbox = $source.is('input[type="checkbox"]');
+        const issourceRadio = $source.is('input[type="radio"]');
+
+        if (isSourceCheckbox || issourceRadio) {
+            const $eventTarget = issourceRadio ? $(`input[name="${$source.attr('name')}"]`) : $source;
+            const toggleClass = () => {
+                $target.toggleClass(this.options.className, $source.is(':checked'));
+            };
+
+            $eventTarget.on('change', (event: JQuery.Event) => { toggleClass(); });
+            toggleClass();
+        } else {
+            $source.on('click', (event: JQuery.Event) => {
+                event.preventDefault();
+                $target.toggleClass(this.options.className);
+            });
+        }
     }
 }
