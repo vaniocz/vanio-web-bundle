fbd7bb9dd7102b0f1306e9cae53e5ad0d59f93a0
davidv <david.vondrak@vanio.cz>
2018-05-27 14:29:40 +0200

    Fixed tests.

--- Tests/Request/RefererHelperTraitTest.php
+++ Tests/Request/RefererHelperTraitTest.php
@@ -8,20 +8,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Vanio\WebBundle\Request\RefererHelperTrait;
-use Vanio\WebBundle\Request\RefererResolver;
+use Vanio\WebBundle\Request\TargetPathResolver;
 
 class RefererHelperTraitTest extends TestCase
 {
-    /** @var RefererResolver|\PHPUnit_Framework_MockObject_MockObject */
-    private $refererResolverMock;
+    /** @var TargetPathResolver|\PHPUnit_Framework_MockObject_MockObject */
+    private $targetPathResolverMock;
 
     /** @var RequestStack */
     private $requestStack;
 
     protected function setUp()
     {
-        $this->refererResolverMock = $this->createMock(RefererResolver::class);
-        $this->refererResolverMock
+        $this->targetPathResolverMock = $this->createMock(TargetPathResolver::class);
+        $this->targetPathResolverMock
             ->expects($this->any())
             ->method('resolveReferer')
             ->willReturnArgument(1);
@@ -31,7 +31,7 @@ class RefererHelperTraitTest extends TestCase
 
     function test_redirecting_to_referer()
     {
-        $refererHelper = new RefererHelper($this->refererResolverMock, $this->requestStack);
+        $refererHelper = new RefererHelper($this->targetPathResolverMock, $this->requestStack);
         $response = $refererHelper->redirectToReferer('referer', Response::HTTP_MOVED_PERMANENTLY, ['key' => 'value']);
 
         $this->assertSame('referer', $response->getTargetUrl());
@@ -44,7 +44,7 @@ class RefererHelperTraitTest extends TestCase
         $refererHelper = new ContainerAwareRefererHelper;
         $container = new Container;
         $container->set('request_stack', $this->requestStack);
-        $container->set('vanio_web.request.referer_resolver', $this->refererResolverMock);
+        $container->set('vanio_web.request.referer_resolver', $this->targetPathResolverMock);
         $refererHelper->setContainer($container);
         $response = $refererHelper->redirectToReferer('referer');
 
@@ -66,9 +66,9 @@ class RefererHelper
         redirectToReferer as public;
     }
 
-    public function __construct(RefererResolver $refererResolver, RequestStack $requestStack)
+    public function __construct(TargetPathResolver $targetPathResolver, RequestStack $requestStack)
     {
-        $this->refererResolver = $refererResolver;
+        $this->targetPathResolver = $targetPathResolver;
         $this->requestStack = $requestStack;
     }
 }
