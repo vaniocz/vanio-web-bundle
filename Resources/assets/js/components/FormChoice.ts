cada1c876f5b83251bc7598fd6e37c685e4a67b6
davidv <david.vondrak@vanio.cz>
2018-12-04 15:38:57 +0100

    Modify FormChoice to enable usage with radio buttons too.

--- Resources/assets/js/components/FormChoice.ts
+++ Resources/assets/js/components/FormChoice.ts
@@ -5,6 +5,7 @@ export default class FormChoice
 {
     private $element: JQuery;
     private $choice: JQuery;
+    private isChoiceSelect: boolean;
     private $form: JQuery;
     private forms: {[choiceValue: string]: JQuery} = {};
     private choiceValue: string;
@@ -13,9 +14,10 @@ export default class FormChoice
     {
         this.$element = $(element);
         this.$choice = this.$element.find('.form-choice__choice');
+        this.isChoiceSelect = this.$choice.is('select');
         this.$form = this.$element.find('.form-choice__form');
         this.$choice.change(this.switchForm.bind(this));
-        this.choiceValue = this.$choice.val() as string;
+        this.choiceValue = this.getChoiceValue();
         this.switchForm();
     }
 
@@ -25,14 +27,26 @@ export default class FormChoice
             this.forms[this.choiceValue] = this.$form.children();
         }
 
-        this.choiceValue = this.$choice.val() as string;
+        this.choiceValue = this.getChoiceValue();
         this.$form.children().detach();
 
         if (this.forms[this.choiceValue]) {
             this.$form.append(this.forms[this.choiceValue]);
         } else {
-            this.$form.append($(this.$choice.find('option:selected').data('form')));
+            this.$form.append(this.getSelectedChoiceForm());
             register(this.$form);
         }
+    }
+
+    private getChoiceValue(): string
+    {
+        return this.isChoiceSelect
+            ? this.$choice.val() as string
+            : this.$choice.find('input:checked').val() as string;
+    }
+
+    private getSelectedChoiceForm(): JQuery
+    {
+        return $(this.$choice.find(this.isChoiceSelect ? 'option:selected' : 'input:checked').data('form'));
     }
 }
