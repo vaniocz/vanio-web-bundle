afb1e5984c5bd98085948901624a527926c61cbb
Marek Štípek <mario.dweller@seznam.cz>
2019-02-27 16:14:39 +0100

    Fixing IntegerType

--- Form/IntegerType.php
+++ Form/IntegerType.php
@@ -5,6 +5,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class IntegerType extends AbstractType implements DataTransformerInterface
 {
@@ -17,25 +18,34 @@ class IntegerType extends AbstractType implements DataTransformerInterface
         $builder->addViewTransformer($this);
     }
 
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefault('compound', false);
+    }
+
     /**
      * @param int|float|string|null $value
      * @return string
      */
     public function transform($value): string
     {
-        if (null === $value) {
+        if ($value === null) {
             return '';
         } else if (!is_int($value) && !ctype_digit($value)) {
             throw new TransformationFailedException('Expected an integer or a string of numeric characters only.');
         }
 
-        return $value;
+        return (string) $value;
     }
 
-    public function reverseTransform(string $value): ?int
+    /**
+     * @param string $value
+     * @return int|null
+     */
+    public function reverseTransform($value): ?int
     {
-        if ('' === $value) {
-            return;
+        if ($value === '') {
+            return null;
         } elseif (!ctype_digit($value)) {
             throw new TransformationFailedException('Expected a string of numeric characters only.');
         }
