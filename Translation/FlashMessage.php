0b7084c27e57c3545c00038957f1e01fff6f9b0d
davidv <david.vondrak@vanio.cz>
2019-03-22 18:43:47 +0100

    Allow adding FlashMessages in controllers with yield statement.

--- Translation/FlashMessage.php
+++ Translation/FlashMessage.php
@@ -9,6 +9,9 @@ class FlashMessage
     const TYPE_DANGER = 'danger';
 
     /** @var string */
+    private $type;
+
+    /** @var string */
     private $message;
 
     /** @var array */
@@ -20,12 +23,38 @@ class FlashMessage
     /** @var string|null */
     private $locale;
 
-    public function __construct(string $message, array $parameters = [], string $domain = null, string $locale = null)
+    public function __construct(string $type, string $message, array $parameters = [], string $domain = null, string $locale = null)
     {
+        $this->type = $type;
         $this->message = $message;
         $this->parameters = $parameters;
         $this->domain = $domain;
         $this->locale = $locale;
+    }
+
+    public static function success(string $message, array $parameters = [], string $domain = null, string $locale = null): self
+    {
+        return new self(self::TYPE_SUCCESS, $message, $parameters, $domain, $locale);
+    }
+
+    public static function info(string $message, array $parameters = [], string $domain = null, string $locale = null): self
+    {
+        return new self(self::TYPE_INFO, $message, $parameters, $domain, $locale);
+    }
+
+    public static function warning(string $message, array $parameters = [], string $domain = null, string $locale = null): self
+    {
+        return new self(self::TYPE_WARNING, $message, $parameters, $domain, $locale);
+    }
+
+    public static function danger(string $message, array $parameters = [], string $domain = null, string $locale = null): self
+    {
+        return new self(self::TYPE_DANGER, $message, $parameters, $domain, $locale);
+    }
+
+    public function type(): string
+    {
+        return $this->type;
     }
 
     public function message(): string
