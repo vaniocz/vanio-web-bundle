b9371d4bbea4bf8492564f7da53be0cafd0a71ef
davidv <david.vondrak@vanio.cz>
2020-01-08 10:54:56 +0100

    Popover - fix of clicking on links inside popovers, added option for setting default link target.

--- Resources/assets/js/components/Popover.ts
+++ Resources/assets/js/components/Popover.ts
@@ -3,19 +3,34 @@ import {component} from 'jquery-ts-components';
 @component('Popover')
 export default class Popover
 {
-    private $element: JQuery;
+    private defaultLinkTarget: string|undefined;
 
-    public constructor(element: JQuery|HTMLElement|string)
+    public constructor(element: JQuery|HTMLElement|string, defaultLinkTarget?: string)
     {
-        this.$element = $(element);
-        this.$element.popover({
-            animation: false,
-            selector: '[data-toggle="popover"]',
-            container: 'body',
-            placement: 'auto bottom' as any,
-            trigger: 'focus',
-            delay: {show: 0, hide: 100},
-            html: true,
-        });
+        $(element)
+            .popover({
+                animation: false,
+                selector: '[data-toggle="popover"]',
+                container: 'body',
+                placement: 'auto bottom' as any,
+                trigger: 'focus',
+                html: true,
+            })
+            .on('mousedown', '.popover', this.onPopoverMouseDown.bind(this));
+
+        this.defaultLinkTarget = defaultLinkTarget;
+    }
+
+    private onPopoverMouseDown(event: JQuery.Event): void
+    {
+        const $target = $(event.target);
+
+        if ($target.is('a')) {
+            event.preventDefault();
+
+            if (!$target.attr('target') && this.defaultLinkTarget) {
+                $target.attr('target', this.defaultLinkTarget);
+            }
+        }
     }
 }
