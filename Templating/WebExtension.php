b9326697733679cfd371598818f84838e0446eeb
maryo <mario.dweller@seznam.cz>
2020-03-25 22:21:04 +0100

    Support protocolless urls for image_dimensions / imagine_dimensions twig functions

--- Templating/WebExtension.php
+++ Templating/WebExtension.php
@@ -386,7 +386,10 @@ class WebExtension extends \Twig_Extension implements \Twig_Extension_GlobalsInt
         }
 
         if (!isset($imageDimensions[$path])) {
-            @$imageDimensions[$path] = getimagesize($path) ?: [0, 0];
+            $normalizedPath = Strings::startsWith($path, '//')
+                ? sprintf('%s:%s', $this->requestStack->getMasterRequest()->getScheme(), $path)
+                : $path;
+            @$imageDimensions[$path] = getimagesize($normalizedPath) ?: [0, 0];
             list($imageDimensions[$path]['width'], $imageDimensions[$path]['height']) = $imageDimensions[$path];
         }
 
